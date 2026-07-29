8988c8c197339fb50547bbe069a8316e08675173
sanite1 <csanni52@gmail.com>
2026-07-29 21:08:33 +0100

    fix: swap admin and app dev ports back to 3001 and 3002

--- src/lib/network/helpers/getModule.ts
+++ src/lib/network/helpers/getModule.ts
@@ -4,7 +4,7 @@ export function getModule(): AppModule {
   const hostname = window.location.hostname
   const port = window.location.port
 
-  if (hostname === 'admin.mensaproducts.com' || port === '3002') return 'admin'
-  if (hostname === 'app.mensaproducts.com' || port === '3001') return 'app'
+  if (hostname === 'admin.mensaproducts.com' || port === '3001') return 'admin'
+  if (hostname === 'app.mensaproducts.com' || port === '3002') return 'app'
   return 'platform'
 }
